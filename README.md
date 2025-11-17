f64fc353d981a30be6876221e13d10d4eab85b7a
Cristian Dario Martinez <crmartinez10@uan.edu.co>
2025-11-17 01:20:50 -0500

    Primer codigo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DFS
 Algoritmo de busqueda profunda DFS
+
+Se solicita Matriz de adyacencia del grafo principal 
+
+Entonces suponiendo que en el programa ingreso la matriz adyacente de un grafo, crea un arreglo, lista o vector en donde dado el nodo (con números, ej nodo numero 1, entonce busco directamente en la posiciona 1 del arreglo) averiguo si esta en -1, 0 u +1 (No visitado, en proceso, visitado)
+
+Solicita vertice inicial
+
+Da como resultado lista de vertices que se alcanzan = {c,a,b,d} y lista de aristas {(c, a), (a, b), (a, d), (a, c) }
+
+La pila se llena lo maximo posible subiendo y vajando de nivel para despues baciarla
+Para compilar en el mismo directorio ejecutar en la terminal "sbcl --script main.lisp" con lisp sbcl previamente adquirido 
---
+++ main.lisp
@@ -0,0 +1,111 @@
+(format t "******************************~%")
+(format t "*  DDDDD   FFFFFFF   SSSSS   *~%")
+(format t "*  D    D  F        S     S  *~%")
+(format t "*  D     D F        S        *~%")
+(format t "*  D     D FFFFF     SSSSS   *~%")
+(format t "*  D     D F              S  *~%")
+(format t "*  D    D  F        S     S  *~%")
+(format t "*  DDDDD   F         SSSSS   *~%")
+(format t "******************************~%")
+
+(defparameter *adyacencia_matriz* (make-array '(30 30) :initial-element 0))
+(defparameter *acceso_matriz* (make-array '(30 30) :initial-element 0))
+(defparameter *vector_estados* (make-array 30 :initial-element -1)) ;{-1, -1, -1, -1, -1} 
+
+(defparameter *numero_vertices* 0) 
+(defparameter *vertice_inicial* 0)
+
+(defparameter *lista_vertices_alcanzables* '()) ;()
+(defparameter *lista_aristas* '()) ;({})
+(defparameter *pila* '())
+
+(defun main()
+    (format t "Hola, este es un programa para uasar DFS ~%")
+    (definir_numero_vertices)
+    (insertar_matriz_adyacencia)
+    (definir_vertice_inicial)
+    (push *vertice_inicial* *pila*) ;Agrega a la pila el nodo inicial
+    (setf (aref *vector_estados* *vertice_inicial*) 0) ;Modifica el estado del nodo inicial a descubierto o 0 del vector de estados en
+    (setf *lista_vertices_alcanzables* (append *lista_vertices_alcanzables* (list *vertice_inicial*))) ;Agrgar a la lista de nodos alcanzables el nodo inicial
+    (format t "Pila = ~a ~%" *pila*)
+    (format t "el estado del nodo inicial = ~a ~%" (aref *vector_estados* *vertice_inicial*))
+    (format t "Lista de nodos alcanzables = ~a ~%" *lista_vertices_alcanzables*)
+    (dfs (car *pila*))
+    (format t "********RESULTADO********~%")
+    (imprimir_vector_estados)
+    (format t "el estado del nodo inicial = ~a ~%" (aref *vector_estados* *vertice_inicial*))
+    (format t "Lista de nodos alcanzables = ~a ~%" *lista_vertices_alcanzables*)
+    ;(format t "Lista de aristas = ~a ~%" *lista_aristas*)
+    (imprimir_lista_aristas)
+)
+
+(defun definir_numero_vertices()
+    (format t "Ingrese el numero de vertices del grafo ~%")
+    (setf *numero_vertices* (read)) 
+)
+
+(defun definir_vertice_inicial()
+    (format t "Ingrese el vertice por el que iniciara DFS ~%")
+    (setf *vertice_inicial* (read))
+)
+
+(defun insertar_matriz_adyacencia()
+    (loop for i from 0 to (- *numero_vertices* 1) do
+        (loop for j from 0 to (- *numero_vertices* 1) do
+            (format t "Ingrese el valor para la posicion i: ~a j: ~a ~%" i j)
+            (setf (aref *adyacencia_matriz* i j) (read))
+        )
+    )
+)
+
+(defun imprimir_vector_estados ()
+    (format t "Vector de estados = ") 
+    (loop for j from 0 to (- *numero_vertices* 1) do
+        (format t "~a " (aref *vector_estados* j))  
+    )
+    (format t "~%")  
+) 
+
+(defun imprimir_lista_aristas ()
+    (let ((anterior 0) (contador 0))  
+        (format t "Lista de aristas = ")
+        (loop for j in *lista_aristas* do 
+            (if (= (mod contador 2) 1)
+                (format t "{~a,~a}, " anterior j) 
+                    (setf anterior j) ;else  
+            )
+            ;(format t "j{~a} " j)
+            ;(format t "anterior{~a} " anterior) 
+            (incf contador)
+        )
+    )
+    (format t "~%")     
+)
+
+(defun dfs (numero_en_pila)
+    (loop for j from 0 to (- *numero_vertices* 1) do
+            (when (= (aref *adyacencia_matriz* numero_en_pila j) 1)
+                (setf (aref *adyacencia_matriz* numero_en_pila j) 0)
+                (when (= (aref *vector_estados* j) -1)
+                    (setf (aref *vector_estados* j) 0)
+                    (push j *pila*)
+                    (setf *lista_vertices_alcanzables* (append *lista_vertices_alcanzables* (list j)))  
+                    (setf *lista_aristas* (append *lista_aristas* (list numero_en_pila)))
+                    (setf *lista_aristas* (append *lista_aristas* (list j)))
+                    (format t "Pila = ~a ~%" *pila*)  
+                    (format t "Lista de nodos alcanzables ~a ~%" *lista_vertices_alcanzables*)
+                    (format t "Lista de aristas ~a ~%" *lista_aristas*)
+                )
+                (unless (null *pila*)
+                    (dfs (car *pila*))
+                )
+            )
+    )
+    (setf (aref *vector_estados* numero_en_pila) 1)
+    (when *pila*
+        (pop *pila*)
+    )
+    ;(format t "Pila = ~a ~%" *pila*)
+)
+
+(main)
